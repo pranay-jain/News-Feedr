67ef693847be9d30e1b8109dfee7b6bf8bda3979
Pranay Jain <pranayjain@dhcp-206-87-209-169.ubcsecure.wireless.ubc.ca>
2016-12-03 14:07:45 -0800

    set basic app validaiton

---
+++ app.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const 
+  bodyParser = require('body-parser'),
+  express = require('express'),
+  https = require('https');
+
+// app is express server
+var app = express();
+app.set('port', process.env.PORT || 5000);
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+
+// App Credentials
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+  process.env.MESSENGER_APP_SECRET :
+  config.get('appSecret');
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+  (process.env.MESSENGER_VALIDATION_TOKEN) :
+  config.get('validationToken');
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  config.get('pageAccessToken');
+
+// URL where the app is running (include protocol). Used to point to scripts and 
+// assets located at this address. 
+const SERVER_URL = (process.env.SERVER_URL) ?
+  (process.env.SERVER_URL) :
+  config.get('serverURL');
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
+
+app.post('/webhook', (req, res) => {
+	res.sendStatus(200);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
