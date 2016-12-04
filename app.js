01f6d7eaf9521e9057ebdf53cdb93b6718a98844
Abdo Megahed <abdohmegahed@dhcp-128-189-239-82.ubcsecure.wireless.ubc.ca>
2016-12-03 16:10:58 -0800

    upload 4

--- app.js
+++ app.js
@@ -10,45 +10,45 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
-// App Credentials
-const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
-  process.env.MESSENGER_APP_SECRET :
-  config.get('appSecret');
-
-// Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-  (process.env.MESSENGER_VALIDATION_TOKEN) :
-  config.get('validationToken');
-
-// Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-  config.get('pageAccessToken');
-
-// URL where the app is running (include protocol). Used to point to scripts and
-// assets located at this address.
-const SERVER_URL = (process.env.SERVER_URL) ?
-  (process.env.SERVER_URL) :
-  config.get('serverURL');
-
-if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
-  console.error("Missing config values");
-  process.exit(1);
-}
+// // App Credentials
+// const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+//   process.env.MESSENGER_APP_SECRET :
+//   config.get('appSecret');
+//
+// // Arbitrary value used to validate a webhook
+// const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+//   (process.env.MESSENGER_VALIDATION_TOKEN) :
+//   config.get('validationToken');
+//
+// // Generate a page access token for your page from the App Dashboard
+// const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+//   (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+//   config.get('pageAccessToken');
+//
+// // URL where the app is running (include protocol). Used to point to scripts and
+// // assets located at this address.
+// const SERVER_URL = (process.env.SERVER_URL) ?
+//   (process.env.SERVER_URL) :
+//   config.get('serverURL');
+//
+// if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+//   console.error("Missing config values");
+//   process.exit(1);
+// }
 
 app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
-app.post('/webhook/', (req, res) => {
-	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === process.env.VALIDATION_TOKEN) {
-	    console.log("Validating webhook");
-	    res.status(200).send(req.query['hub.challenge']);
-	} else {
-		console.error("Failed validation. Make sure the validation tokens match.");
-    	res.sendStatus(403);
-	}
-});
+// app.post('/webhook/', (req, res) => {
+// 	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === process.env.VALIDATION_TOKEN) {
+// 	    console.log("Validating webhook");
+// 	    res.status(200).send(req.query['hub.challenge']);
+// 	} else {
+// 		console.error("Failed validation. Make sure the validation tokens match.");
+//     	res.sendStatus(403);
+// 	}
+// });
 
 
 app.listen(app.get('port'), function() {
