b51bb44ef55ccd92aa74041f8d63e498a61f166b
Abdo Megahed <abdohmegahed@dhcp-128-189-239-82.ubcsecure.wireless.ubc.ca>
2016-12-03 17:18:46 -0800

    whatever

--- index.js
+++ index.js
@@ -37,29 +37,29 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
-            //sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            sendTextMessage(sender, "Text received, bitch: " + text.substring(0, 200))
         }
     }
     res.sendStatus(200)
 });
 
-// function sendTextMessage(sender, text) {
-//     let messageData = { text:text }
-//     request({
-//         url: 'https://graph.facebook.com/v2.6/me/messages',
-//         qs: {access_token:token},
-//         method: 'POST',
-//         json: {
-//             recipient: {id:sender},
-//             message: messageData,
-//         }
-//     }, function(error, response, body) {
-//         if (error) {
-//             console.log('Error sending messages: ', error)
-//         } else if (response.body.error) {
-//             console.log('Error: ', response.body.error)
-//         }
-//     })
-// }
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
 
 const token = process.env.FB_PAGE_ACCESS_TOKEN
